90cd98a22468cdb86d0548b82c9eaf4ccad3ef56
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-01 13:36:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,3 +38,20 @@ curl "https://arxiv.gg/api/v1/papers/1706.03762/graph"
 
 # Export as BibTeX
 curl "https://arxiv.gg/api/v1/papers/1706.03762/export/bibtex"
+git clone https://github.com/josStorer/RWKV-Runner
+
+# Then
+cd RWKV-Runner
+python ./backend-python/main.py #The backend inference service has been started, request /switch-model API to load the model, refer to the API documentation: http://127.0.0.1:8000/docs
+
+# Or
+cd RWKV-Runner/frontend
+npm ci
+npm run build #Compile the frontend
+cd ..
+python ./backend-python/webui_server.py #Start the frontend service separately
+# Or
+python ./backend-python/main.py --webui #Start the frontend and backend service at the same time
+
+# Help Info
+python ./backend-python/main.py -h
